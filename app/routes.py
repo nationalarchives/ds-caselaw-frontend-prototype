f9bfb0f9264432aefb47c4da5e65537047e377dc
Gwyn Jones <gtvj@users.noreply.github.com>
2021-11-30 14:38:35 +0000

    Add basic search form

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,12 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 from content.recent_judgments import recent_judgments
 from content.service_wide import service
 
 @app.route('/')
 def home():
     return render_template('home.html', service=service, recent_judgments=recent_judgments)
+
+@app.route('/search')
+def search():
+    return request.args['query']
