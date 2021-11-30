4156055162a20c388026486abe0d3cd57c93fa00
Gwyn Jones <gtvj@users.noreply.github.com>
2021-11-30 17:36:57 +0000

    Add collections

--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,19 @@ from app import app
 from flask import render_template, request
 from content.recent_judgments import recent_judgments
 from content.service_wide import service
+from content.collections import collections
+
 
 @app.route('/')
 def home():
-    return render_template('home.html', service=service, recent_judgments=recent_judgments)
+    return render_template(
+        'home.html',
+        service=service,
+        recent_judgments=recent_judgments,
+        collections=collections
+    )
 
-@app.route('/search')
+
+@app.route('/search', methods=['GET'])
 def search():
-    return request.args['query']
+    return request.args['collection']
---
+++ content/collections.py
@@ -0,0 +1,59 @@
+collections = [
+    {
+        'name': 'Court of Appeal (Civil Division)',
+        'code': 'ewca/civ'
+    },
+    {
+        'name': 'Court of Appeal (Criminal Division)',
+        'code': 'ewca/crim'
+    },
+    {
+        'name': 'Court of Protection',
+        'code': 'ewcop'
+    },
+    {
+        'name': 'Family Court',
+        'code': 'ewfc'
+    },
+    {
+        'name': 'High Court (Administrative Court)',
+        'code': 'ewhc/admin'
+    },
+    {
+        'name': 'High Court (Admiralty Court)',
+        'code': 'ewhc/admlty'
+    },
+    {
+        'name': 'High Court (Chancery Division)',
+        'code': 'ewhc/ch'
+    },
+    {
+        'name': 'High Court (Commercial Court)',
+        'code': 'ewhc/comm'
+    },
+    {
+        'name': 'High Court (Supreme Court Costs Office)',
+        'code': 'ewhc/costs'
+    },
+    {
+        'name': 'High Court (Family Division)',
+        'code': 'ewhc/fam'
+    },
+    {
+        'name': 'High Court (Intellectual Property Enterprise Court)',
+        'code': 'ewhc/ipec'
+    },
+    {
+        'name': 'High Court (Patents Court)',
+        'code': 'ewhc/pat'
+    },
+    {
+        'name': 'High Court (Queens Bench Division)',
+        'code': 'ewhc/qb'
+    },
+    {
+        'name': 'High Court (Technology and Construction Court)',
+        'code': 'ewhc/tcc'
+    }
+
+]
