5d01e184873fcd90cefb4f1a72e6a34bc10a71e2
Gwyn Jones <gtvj@users.noreply.github.com>
2021-11-30 13:20:17 +0000

    Add recent judgments

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from app import app
 from flask import render_template
-
+from content.recent_judgments import recent_judgments
+from content.service_wide import service
 
 @app.route('/')
 def home():
-    return render_template('home.html', title="Judgments service front end")
+    return render_template('home.html', service=service, recent_judgments=recent_judgments)
---
+++ content/recent_judgments.py
@@ -0,0 +1,85 @@
+recent_judgments = [
+    {
+        "title": "REGINA v Peter HUNTER",
+        "neutral_citation": "[2021] EWCA Crim 1785"
+    },
+    {
+        "title": "MALIK RIAZ HUSSAIN v SECRETARY OF STATE FOR THE HOME DEPARTMENT",
+        "neutral_citation": "[2021] EWCA Civ 2781"
+    },
+    {
+        "title": "Ciftci v London Borough of Haringey",
+        "neutral_citation": "[2021] EWCA Civ 1772"
+    },
+    {
+        "title": "JOHN MCKEOWN v DIANA LANGER",
+        "neutral_citation": "[2021] EWCA Civ 1792"
+    },
+    {
+        "title": "WENDY CARRINGTON v SECRETARY OF STATE FOR WORK AND PENSIONS",
+
+        "neutral_citation": "[2021] EWCA Civ 1724"
+    },
+    {
+        "title": "Re AH",
+        "neutral_citation": "[2021] EWCA Civ 1768"
+    },
+    {
+        "title": "R v Ahmed",
+        "neutral_citation": "[2021] EWCA Crim 1786"
+    },
+    {
+        "title": "EUI LIMITED v UK VODAPHONE LIMITED",
+        "neutral_citation": "[2021] EWCA Civ 1771"
+    },
+    {
+        "title": "The Motherhood Plan (R on the application of) v HM Treasury",
+
+        "neutral_citation": "[2021] EWCA Civ 1703"
+    },
+    {
+        "title": "THE SECRETARY OF STATE FOR THE HOME DEPARTMENT v RM (PAKISTAN)",
+
+        "neutral_citation": "[2021] EWCA Civ 1754"
+    },
+    {
+        "title": "Re A",
+        "neutral_citation": "[2021] EWCA Civ 1749"
+    },
+    {
+        "title": "KENSQUARE LIMITED v MARY ADWOA AKYAA BOAKYE",
+        "neutral_citation": "[2021] EWCA Civ 1725"
+    },
+    {
+        "title": "Cunningham v Rochdale Metropolitan Bor Council",
+        "neutral_citation": "[2021] EWCA Civ 1719"
+    },
+    {
+        "title": "R v Jex",
+        "neutral_citation": "[2021] EWCA Crim 1708"
+    },
+    {
+        "title": "THE QUEEN v LLOYD KELLY",
+        "neutral_citation": "[2021] EWCA CIV 1699"
+    },
+    {
+        "title": "KEYHOLE BRIDGE USER SAFETY GROUP (R on the application of) v BOURNEMOUTH, CHRISTCHURCH AND POOLE COUNCIL",
+        "neutral_citation": "[2021] EWHC 3082 (Admin)"
+    },
+    {
+        "title": "K LINE PTE LIMITED v PRIMINDS SHIPPING (HK) CO LIMITED",
+        "neutral_citation": "[2021] EWCA Civ 1712"
+    },
+    {
+        "title": "PROMONTORIA (OAK) LTD v NICHOLAS MICHAEL EMANUEL",
+        "neutral_citation": "[2021] EWCA Civ 1682"
+    },
+    {
+        "title": "DEORANEE BOODIA v VOLODYMRY YATSYNA",
+        "neutral_citation": "[2021] EWCA Civ 1705"
+    },
+    {
+        "title": "CHRISTIAN FEARON v REGINA",
+        "neutral_citation": "[2021] EWCA Crim 1706"
+    }
+]
