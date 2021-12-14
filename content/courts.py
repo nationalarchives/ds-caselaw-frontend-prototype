5123a93ae75f037402825db806e9afa99fc6ea5f
Gwyn Jones <gtvj@users.noreply.github.com>
2021-12-14 14:50:40 +0000

    Add sorting for browse by court and streamline logic for court presentation

--- content/courts.py
+++ content/courts.py
@@ -2,106 +2,94 @@ courts = [
     {
         'name': 'United Kingdom Supreme Court',
         'code': 'uksc',
-        'subdivisions': [],
         'years_range_start': 2021,
         'years_range_end': 2021
     },
     {
         'name': 'Privy Council',
         'code': 'ukpc',
-        'subdivisions': [],
         'years_range_start': 2021,
         'years_range_end': 2021
     },
+
     {
-        'name': 'Court of Appeal',
-        'code': 'ewca',
-        'subdivisions': [
-            {
-                'name': 'Court of Appeal Civil Division',
-                'code': 'ewca/civ',
-                'years_range_start': 2003,
-                'years_range_end': 2021
-            },
-            {
-                'name': 'Court of Appeal Criminal Division',
-                'code': 'ewca/crim',
-                'years_range_start': 2003,
-                'years_range_end': 2021
-            },
-        ]
+        'name': 'Court of Appeal Civil Division',
+        'code': 'ewca/civ',
+        'years_range_start': 2003,
+        'years_range_end': 2021
     },
     {
-        'name': 'High Court',
-        'code': 'ewhc',
-        'subdivisions': [
-            {
-                'name': 'Administrative Court',
-                'code': 'ewhc/admin',
-                'years_range_start': 2003,
-                'years_range_end': 2021
-            },
-            {
-                'name': 'Admiralty Court',
-                'code': 'ewhc/admlty',
-                'years_range_start': 2003,
-                'years_range_end': 2021
-            },
-            {
-                'name': 'Chancery Division of the High Court',
-                'code': 'ewhc/ch',
-                'years_range_start': 2003,
-                'years_range_end': 2021
-            },
-            {
-                'name': 'Commercial Court',
-                'code': 'ewhc/comm',
-                'years_range_start': 2003,
-                'years_range_end': 2021
-            },
-            {
-                'name': 'Senior Court Costs Office',
-                'code': 'ewhc/costs',
-                'years_range_start': 2003,
-                'years_range_end': 2021
-            },
-            {
-                'name': 'Family Division of the High Court',
-                'code': 'ewhc/fam',
-                'years_range_start': 2003,
-                'years_range_end': 2021
-            },
-            {
-                'name': 'Intellectual Property Enterprise Court',
-                'code': 'ewhc/ipec',
-                'years_range_start': 2013,
-                'years_range_end': 2021
-            },
-            {
-                'name': 'Mercantile Court',
-                'code': 'ewhc/mercantile',
-                'years_range_start': 2008,
-                'years_range_end': 2015
-            },
-            {
-                'name': 'Patents Court',
-                'code': 'ewhc/pat',
-                'years_range_start': 2003,
-                'years_range_end': 2021
-            },
-            {
-                'name': 'Queen\'s Bench Division of the High Court',
-                'code': 'ewhc/qb',
-                'years_range_start': 2003,
-                'years_range_end': 2021
-            },
-            {
-                'name': 'Technology and Construction Court',
-                'code': 'ewhc/tcc',
-                'years_range_start': 2003,
-                'years_range_end': 2021
-            },
-        ]
+        'name': 'Court of Appeal Criminal Division',
+        'code': 'ewca/crim',
+        'years_range_start': 2003,
+        'years_range_end': 2021
+    },
+
+    {
+        'name': 'Administrative Court',
+        'code': 'ewhc/admin',
+        'years_range_start': 2003,
+        'years_range_end': 2021
+    },
+    {
+        'name': 'Admiralty Court',
+        'code': 'ewhc/admlty',
+        'years_range_start': 2003,
+        'years_range_end': 2021
+    },
+    {
+        'name': 'Chancery Division of the High Court',
+        'code': 'ewhc/ch',
+        'years_range_start': 2003,
+        'years_range_end': 2021
+    },
+    {
+        'name': 'Commercial Court',
+        'code': 'ewhc/comm',
+        'years_range_start': 2003,
+        'years_range_end': 2021
+    },
+    {
+        'name': 'Senior Court Costs Office',
+        'code': 'ewhc/costs',
+        'years_range_start': 2003,
+        'years_range_end': 2021
+    },
+    {
+        'name': 'Family Division of the High Court',
+        'code': 'ewhc/fam',
+        'years_range_start': 2003,
+        'years_range_end': 2021
+    },
+    {
+        'name': 'Intellectual Property Enterprise Court',
+        'code': 'ewhc/ipec',
+        'years_range_start': 2013,
+        'years_range_end': 2021
+    },
+    {
+        'name': 'Mercantile Court',
+        'code': 'ewhc/mercantile',
+        'years_range_start': 2008,
+        'years_range_end': 2015
+    },
+    {
+        'name': 'Patents Court',
+        'code': 'ewhc/pat',
+        'years_range_start': 2003,
+        'years_range_end': 2021
+    },
+    {
+        'name': 'Queen\'s Bench Division of the High Court',
+        'code': 'ewhc/qb',
+        'years_range_start': 2003,
+        'years_range_end': 2021
+    },
+    {
+        'name': 'Technology and Construction Court',
+        'code': 'ewhc/tcc',
+        'years_range_start': 2003,
+        'years_range_end': 2021
     },
     {
         'name': 'Court of Protection',
