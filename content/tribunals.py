0537b5d981b98b7e02ee4c8fbaaff449249a85cc
Gwyn Jones <gtvj@users.noreply.github.com>
2022-05-12 10:43:19 +0100

    Introduce potential for Tribunals to be represented on the home page

---
+++ content/tribunals.py
@@ -0,0 +1,22 @@
+tribunals = [
+    {
+        "name": "Upper Tribunal (Tax and Chancery Chamber)",
+        "reference": "[2022] UKUT 123 (TCC)"
+    },
+    {
+        "name": "Upper Tribunal (Lands Chamber)",
+        "reference": "[2022] UKUT 123 (LC)"
+    },
+    {
+        "name": "Upper Tribunal (Immigration and Asylum Chamber)",
+        "reference": "[2022] UKUT 123 (IAC)"
+    },
+    {
+        "name": "Upper Tribunal (Administrative Appeals Chamber)",
+        "reference": "[2022] UKUT 123 (AAC)"
+    },
+    {
+        "name": "United Kingdom Employment Appeal Tribunal",
+        "reference": "[2022] EAT 123"
+    }
+]
