5d7f2608becc602a38694b5efe6ad5443e8acbba
Matt Blair <7143978+matt-blair@users.noreply.github.com>
2022-05-25 17:21:20 +0100

    Restyling homepage browse, homepage styles, judgment button labels and adding year range to tribunal list

--- content/courts.py
+++ content/courts.py
@@ -2,86 +2,86 @@ courts = [
   {
     "name": "United Kingdom Supreme Court",
     "code": "uksc",
-    "years": "(2021 &ndash; 2021)"
+    "years": "2021 &ndash; 2022"
   },
   {
     "name": "Privy Council",
     "code": "ukpc",
-    "years": "(2021 &ndash; 2021)"
+    "years": "2021 &ndash; 2022"
   },
   {
     "name": "Court of Appeal Civil Division",
     "code": "civ",
-    "years": "(2003 &ndash; 2021)"
+    "years": "2003 &ndash; 2022"
   },
   {
     "name": "Court of Appeal Criminal Division",
     "code": "crim",
-    "years": "(2003 &ndash; 2021)"
+    "years": "2003 &ndash; 2022"
   },
   {
     "name": "Administrative Court",
     "code": "admin",
-    "years": "(2003 &ndash; 2021)"
+    "years": "2003 &ndash; 2022"
   },
   {
     "name": "Admiralty Court",
     "code": "admlty",
-    "years": "(2003 &ndash; 2021)"
+    "years": "2003 &ndash; 2022"
   },
   {
     "name": "Chancery Division of the High Court",
     "code": "ch",
-    "years": "(2003 &ndash; 2021)"
+    "years": "2003 &ndash; 2022"
   },
   {
     "name": "Commercial Court",
     "code": "comm",
-    "years": "(2003 &ndash; 2021)"
+    "years": "2003 &ndash; 2022"
   },
   {
     "name": "Senior Court Costs Office",
     "code": "costs",
-    "years": "(2003 &ndash; 2021)"
+    "years": "2003 &ndash; 2022"
   },
   {
     "name": "Family Division of the High Court",
     "code": "fam",
-    "years": "(2003 &ndash; 2021)"
+    "years": "2003 &ndash; 2022"
   },
   {
     "name": "Intellectual Property Enterprise Court",
     "code": "ipec",
-    "years": "(2013 &ndash; 2021)"
+    "years": "2013 &ndash; 2022"
   },
   {
     "name": "Mercantile Court",
     "code": "mercantile",
-    "years": "(2008 &ndash; 2015)"
+    "years": "2008 &ndash; 2015"
   },
   {
     "name": "Patents Court",
     "code": "pat",
-    "years": "(2003 &ndash; 2021)"
+    "years": "2003 &ndash; 2022"
   },
   {
     "name": "Queen's Bench Division of the High Court",
     "code": "qb",
-    "years": "(2003 &ndash; 2021)"
+    "years": "2003 &ndash; 2022"
   },
   {
     "name": "Technology and Construction Court",
     "code": "tcc",
-    "years": "(2003 &ndash; 2021)"
+    "years": "2003 &ndash; 2022"
   },
   {
     "name": "Court of Protection",
     "code": "ewcop",
-    "years": "(2009 &ndash; 2021)"
+    "years": "2009 &ndash; 2022"
   },
   {
     "name": "Family Court",
     "code": "ewfc",
-    "years": "(2014 &ndash; 2021)"
+    "years": "22014 &ndash; 2022"
   }
 ]
--- content/tribunals.py
+++ content/tribunals.py
@@ -1,22 +1,27 @@
 tribunals = [
     {
-        "name": "Upper Tribunal (Tax and Chancery Chamber)",
-        "reference": "[2022] UKUT 123 (TCC)"
-    },
-    {
-        "name": "Upper Tribunal (Lands Chamber)",
-        "reference": "[2022] UKUT 123 (LC)"
+        "name": "Upper Tribunal (Administrative Appeals Chamber)",
+        "reference": "[2022] UKUT 123 (AAC)",
+        "years": "From 2022"
     },
     {
         "name": "Upper Tribunal (Immigration and Asylum Chamber)",
-        "reference": "[2022] UKUT 123 (IAC)"
+        "reference": "[2022] UKUT 123 (IAC)",
+        "years": "From 2022"
     },
     {
-        "name": "Upper Tribunal (Administrative Appeals Chamber)",
-        "reference": "[2022] UKUT 123 (AAC)"
+        "name": "Upper Tribunal (Lands Chamber)",
+        "reference": "[2022] UKUT 123 (LC)",
+        "years": "From 2022"
     },
     {
-        "name": "United Kingdom Employment Appeal Tribunal",
-        "reference": "[2022] EAT 123"
+        "name": "Upper Tribunal (Tax and Chancery Chamber)",
+        "reference": "[2022] UKUT 123 (TCC)",
+        "years": "From 2022"
+    },
+    {
+        "name": "Employment Appeal Tribunal",
+        "reference": "[2022] EAT 123",
+        "years": "2021 &ndash; 2022"
     }
 ]
