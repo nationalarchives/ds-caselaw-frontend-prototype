8ae5011cd06ab4077b535bc351b520132cffbf94
Gwyn Jones <gtvj@users.noreply.github.com>
2022-05-25 18:21:59 +0100

    Update tribunal links on homepage to refer to live site
    This is necessary for user testing scenarios planned for tomorrow.

--- content/tribunals.py
+++ content/tribunals.py
@@ -2,26 +2,31 @@ tribunals = [
     {
         "name": "Upper Tribunal (Administrative Appeals Chamber)",
         "reference": "[2022] UKUT 123 (AAC)",
-        "years": "From 2022"
+        "years": "From 2022",
+        "href": "https://caselaw.nationalarchives.gov.uk/judgments/advanced_search?query=&party=&judge=&neutral_citation=&specific_keyword=&court=ukut&from=&to="
     },
     {
         "name": "Upper Tribunal (Immigration and Asylum Chamber)",
         "reference": "[2022] UKUT 123 (IAC)",
-        "years": "From 2022"
+        "years": "From 2022",
+        "href": "https://caselaw.nationalarchives.gov.uk/judgments/advanced_search?query=&party=&judge=&neutral_citation=&specific_keyword=&court=ukut&from=&to="
     },
     {
         "name": "Upper Tribunal (Lands Chamber)",
         "reference": "[2022] UKUT 123 (LC)",
-        "years": "From 2022"
+        "years": "From 2022",
+        "href": "https://caselaw.nationalarchives.gov.uk/judgments/advanced_search?query=&party=&judge=&neutral_citation=&specific_keyword=&court=ukut&from=&to="
     },
     {
         "name": "Upper Tribunal (Tax and Chancery Chamber)",
         "reference": "[2022] UKUT 123 (TCC)",
-        "years": "From 2022"
+        "years": "From 2022",
+        "href": "https://caselaw.nationalarchives.gov.uk/judgments/advanced_search?query=&party=&judge=&neutral_citation=&specific_keyword=&court=ukut&from=&to="
     },
     {
         "name": "Employment Appeal Tribunal",
         "reference": "[2022] EAT 123",
-        "years": "2021 &ndash; 2022"
+        "years": "2021 &ndash; 2022",
+        "href": "https://caselaw.nationalarchives.gov.uk/judgments/advanced_search?query=&party=&judge=&neutral_citation=&specific_keyword=&court=eat&from=&to="
     }
 ]
